f94c6f118943caeaf95737019d34cb82bf8d99ef
Myller Sakaguchi Lobo <myller.lobo@omni.com.br>
2025-01-12 19:36:18 -0300

    auth api segmentation

--- src/main/java/com/fiap/tc/TcAuthBackendApiApplication.java
+++ src/main/java/com/fiap/tc/TcAuthBackendApiApplication.java
@@ -4,11 +4,6 @@ import com.fiap.tc.infrastructure.core.security.property.OriginApiProperty;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.BufferedImageHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
-
-import java.awt.image.BufferedImage;
 
 
 @SpringBootApplication
@@ -18,11 +13,6 @@ public class TcAuthBackendApiApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(TcAuthBackendApiApplication.class, args);
-    }
-
-    @Bean
-    public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
-        return new BufferedImageHttpMessageConverter();
     }
 
 }
